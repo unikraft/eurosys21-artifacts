ea04245fcdce8631da6128f2f0e664f110fc4cb5
Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
2021-03-01 10:53:49 +0200

    experiment 21: Add parts

--- experiments/25_kvs_compare/run_vhost.sh
+++ experiments/25_kvs_compare/run_vhost.sh
@@ -1,1 +1,1 @@
-taskset -c 2,3 /root/dpdk/build/app/dpdk-testpmd -l 2,3 -n 4 --vdev 'eth_vhost0,iface=vhost-net,queues=1' -- -i --txd=1024 --rxd=1024 --nb-cores=1 --eth-peer=0,52:54:00:12:34:57 --eth-peer=1,52:54:00:12:34:57
+taskset -c 2,3 aux/dpdk/build/app/dpdk-testpmd -l 2,3 -n 4 --vdev 'eth_vhost0,iface=vhost-net,queues=1' -- -i --txd=1024 --rxd=1024 --nb-cores=1 --eth-peer=0,52:54:00:12:34:57 --eth-peer=1,52:54:00:12:34:57
