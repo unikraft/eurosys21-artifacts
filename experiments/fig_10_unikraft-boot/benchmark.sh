a33a9511076059a85f2a01e9a9afba7002c7e7f7
Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
2021-03-02 08:45:58 +0100

    Fig. 10 almost there.
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>

--- experiments/fig_10_unikraft-boot/benchmark.sh
+++ experiments/fig_10_unikraft-boot/benchmark.sh
@@ -12,7 +12,7 @@ QEMU_GUEST=../tools/qemu-guest
 suffix=`date '+%d%m%Y%H%M%S'`
 mkdir -p rawdata/ results
 
-LOG=rawdata/solo5-${suffix}.txt
+LOG=rawdata/solo5-${suffix}.csv
 touch $LOG
 
 for j in {1..1}
@@ -20,7 +20,7 @@ do
 	${IMAGES}/solo5_hvt --mem=2 ${IMAGES}/unikraft+solo5.kernel &> $LOG
 done
 
-LOG=rawdata/firecracker-${suffix}.txt
+LOG=rawdata/firecracker-${suffix}.csv
 touch $LOG
 for j in {1..1}
 do
@@ -35,10 +35,82 @@ do
 		pkill -9 firecracker
 	} &
 
+	checker_pid=$!
+
 	taskset -c ${CPU1},${CPU2} ${IMAGES}/firecracker \
 			--config-file data/firecracker.config \
 			--api-sock firecracker.socket
 
+	wait $checker_pid
+
 	rm firecracker.socket
 	rm logs.fifo metrics.fifo
+done
+
+QEMU_PATH=${IMAGES}/qemu/build/x86_64-softmmu/qemu-system-x86_64
+LOG=rawdata/qemumicrovm-${suffix}.csv
+touch $LOG
+for j in {1..1}
+do
+	{
+		sleep 3
+		pkill -9 qemu-system-x86_64
+	} &
+
+	checker_pid=$!
+
+	taskset -c ${CPU1},${CPU2} ${QEMU_PATH} \
+		-L ${IMAGES}/qemu/pc-bios/ \
+		-enable-kvm -nographic -device isa-debug-exit \
+		-kernel ${IMAGES}/unikraft+qemu.kernel -m 2M \
+		-nodefaults -no-user-config -serial stdio \
+		-M microvm,x-option-roms=off,pit=off,pic=off,rtc=on \
+		-cpu host,migratable=no,+invtsc \
+		-overcommit mem-lock=on &> $LOG
+
+	wait $checker_pid
+done
+
+LOG=rawdata/qemu-${suffix}.csv
+touch $LOG
+for j in {1..1}
+do
+	{
+		sleep 3
+		pkill -9 qemu-system-x86_64
+	} &
+
+	checker_pid=$!
+
+	taskset -c ${CPU1},${CPU2} ${QEMU_PATH} \
+		-L ${IMAGES}/qemu/pc-bios/ \
+		-enable-kvm -nographic -device isa-debug-exit \
+		-kernel ${IMAGES}/unikraft+qemu.kernel -m 2M \
+		-nodefaults -no-user-config -serial stdio \
+		-cpu host,migratable=no,+invtsc -no-reboot &> $LOG
+
+	wait $checker_pid
+done
+
+LOG=rawdata/qemu1nic-${suffix}.csv
+touch $LOG
+for j in {1..1}
+do
+	{
+		sleep 3
+		pkill -9 qemu-system-x86_64
+	} &
+
+	checker_pid=$!
+
+	taskset -c ${CPU1},${CPU2} ${QEMU_PATH} \
+		-L ${IMAGES}/qemu/pc-bios/ \
+		-enable-kvm -nographic -device isa-debug-exit \
+		-kernel ${IMAGES}/unikraft+qemu.kernel -m 2M \
+		-nodefaults -no-user-config -serial stdio \
+		-cpu host,migratable=no,+invtsc -no-reboot \
+		-netdev tap,ifname=qemuif,id=uknetdev,script=no \
+		-device virtio-net-pci,netdev=uknetdev,mac=aa:bb:cc:00:01:01 &> $LOG
+
+	wait $checker_pid
 done
