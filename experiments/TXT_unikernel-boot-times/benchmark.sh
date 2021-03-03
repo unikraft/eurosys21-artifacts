523813c3557fdd5f47d042ce3ea963465e7116dc
Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
2021-03-03 10:36:53 +0100

    Improve TXT_unikernel-boot-times' stability.
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>

--- experiments/TXT_unikernel-boot-times/benchmark.sh
+++ experiments/TXT_unikernel-boot-times/benchmark.sh
@@ -3,6 +3,8 @@
 # verbose output
 set -x
 
+source ../common/set-cpus.sh
+
 IMAGES=$(pwd)/images/
 FIRECRACKER_PATH=$(pwd)/.firecracker
 VMMSOCKET=$(pwd)/.firecracker.socket
@@ -22,18 +24,20 @@ function firecracker_cleanup {
 }
 
 function runlupine {
-    {
-      sleep 5
-      firecracker_cleanup
-      rm ${IMAGES}/lupine-portwrite.ext2.disp
-    } &
-    cp ${IMAGES}/lupine-portwrite.ext2 ${IMAGES}/lupine-portwrite.ext2.disp
-
+    cp ${IMAGES}/lupine-portwrite.ext2 /tmp/lupine-portwrite.ext2.disp
+    cp ${IMAGES}/${1}.img /tmp/${1}.img
     touch .out
-    firectl --firecracker-binary=$(pwd)/.firecracker \
-	--kernel ${IMAGES}/${1}.img \
+
+    {
+      sleep 3
+      firecracker_cleanup
+      rm /tmp/lupine-portwrite.ext2.disp /tmp/${1}.img
+    } &
+
+    taskset -c ${CPU1} firectl --firecracker-binary=$(pwd)/.firecracker \
+	--kernel /tmp/${1}.img \
 	--kernel-opts='ro panic=-1 pci=off reboot=k tsc=reliable quiet 8250.nr_uarts=0 ipv6.disable=1 init=/measure-boot-fc' \
-	--root-drive=${IMAGES}/lupine-portwrite.ext2.disp \
+	--root-drive=/tmp/lupine-portwrite.ext2.disp \
 	--firecracker-log=.out > /dev/null 2> /dev/null
 
     # get CPU ms
@@ -48,14 +52,16 @@ function runvm {
   vm=$1
 
   if [ "$vm" = "alpine-vm" ]; then
-    cp ${IMAGES}/alpine.raw ${IMAGES}/alpine.raw.disposible
+    cp ${IMAGES}/alpine.raw /tmp/alpine.raw.disposible
+
     {
       sleep 8
       firecracker_cleanup
-      rm ${IMAGES}/alpine.raw.disposible
+      rm /tmp/alpine.raw.disposible
     } &
-    firectl --kernel=${IMAGES}/hello-vmlinux.bin \
-	    --root-drive=${IMAGES}/alpine.raw.disposible \
+
+    taskset -c ${CPU1} firectl --kernel=${IMAGES}/hello-vmlinux.bin \
+	    --root-drive=/tmp/alpine.raw.disposible \
 	    --firecracker-binary=$(pwd)/.firecracker \
 	    --firecracker-log=.out > /dev/null 2> /dev/null
     cat .out | grep Guest-boot-time | rev | awk -e '$0 {print $3}' | \
@@ -68,18 +74,21 @@ function runvm {
     runlupine $vm
   elif [ "$vm" = "hermitux" ]; then
     docker pull hlefeuvre/hermitux-boottime > /dev/null 2> /dev/null
-    docker run --privileged hlefeuvre/hermitux-boottime | \
+    docker run --privileged --cpuset-cpus="${CPU1}-${CPU3}" \
+			hlefeuvre/hermitux-boottime | \
 		  awk -e '$0 ~ /([0-9]+)ns/ {print $4}' | \
 		  cut -c 4- | rev | cut -c 3- | rev
   elif [ "$vm" = "hermitux-light" ]; then
     docker pull hlefeuvre/hermitux-light-boottime > /dev/null 2> /dev/null
-    docker run --privileged hlefeuvre/hermitux-light-boottime | \
+    docker run --privileged --cpuset-cpus="${CPU1}-${CPU3}" \
+			hlefeuvre/hermitux-light-boottime | \
 			awk -e '$0 ~ /([0-9]+)ns/ {print $4}' | \
 			cut -c 4- | rev | cut -c 3- | rev
   elif [ "$vm" = "osv-rofs" ]; then
     CONTAINER=osv
     docker pull hlefeuvre/osv > /dev/null 2> /dev/null
     docker run --privileged --name=$CONTAINER \
+			--cpuset-cpus="${CPU1}-${CPU3}" \
 			-dt hlefeuvre/osv > /dev/null 2> /dev/null
     docker exec -it $CONTAINER ./scripts/firecracker.py -c 1 -m 128M | \
 			awk -e '$0 ~ /Booted/ {print int(1000000*($4))}'
@@ -89,7 +98,8 @@ function runvm {
   elif [ "$vm" = "rump" ]; then
     CONTAINER=rump
     docker pull hlefeuvre/rump-solo5-boottime > /dev/null 2> /dev/null
-    docker run --privileged -it hlefeuvre/rump-solo5-boottime |
+    docker run --privileged -it --cpuset-cpus="${CPU1}-${CPU3}" \
+	hlefeuvre/rump-solo5-boottime | \
 	awk -e '$0 ~ /guest startup/ {print $4}' | rev | cut -c 3- | rev |
 	awk '{print int(1000*($0))}'
   elif [ "$vm" = "docker-min" ]; then
@@ -98,7 +108,8 @@ function runvm {
     } &
     child=$!
     docker pull hello-world  > /dev/null 2> /dev/null
-    taskset -c 8 docker run -m 30MB --privileged \
+    docker run -m 30MB --privileged \
+		--cpuset-cpus="${CPU1}-${CPU3}" \
 		-it hello-world > /dev/null 2> /dev/null
     kill $child > /dev/null 2> /dev/null
     LC_NUMERIC="C" grep -P "container (start|die)" .out | tr :+ ' ' | \
