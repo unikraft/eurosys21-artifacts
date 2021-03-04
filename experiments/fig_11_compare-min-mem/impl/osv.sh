fee35208672f14d240d75822bc7b385d422c5297
Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
2021-03-04 14:01:25 +0100

    Fix OSv in Exp. 11.
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>

--- experiments/fig_11_compare-min-mem/impl/osv.sh
+++ experiments/fig_11_compare-min-mem/impl/osv.sh
@@ -8,10 +8,7 @@ source ../common/network.sh
 source ../common/redis.sh
 source ../common/qemu.sh
 
-IMAGES=$(pwd)/images/
-
 function hello_osv_with_mem {
-    IMAGES=$(pwd)/images
     CONTAINER=osv-hello-ramusage
     docker pull hlefeuvre/osv
     docker run --privileged --name=$CONTAINER -dt hlefeuvre/osv
@@ -35,7 +32,6 @@ function hello_osv_with_mem {
 }
 
 function sqlite_osv_with_mem {
-    IMAGES=$(pwd)/images
     CONTAINER=osv-sqlite-ramusage
     docker pull hlefeuvre/osv
     docker run --privileged --name=$CONTAINER -dt hlefeuvre/osv
@@ -60,95 +56,51 @@ function sqlite_osv_with_mem {
 }
 
 function redis_osv_with_mem {
-    IMAGES=$(pwd)/images/redis
-    NETIF=tux0
-    create_bridge $NETIF $BASEIP
-    dnsmasq_pid=$(run_dhcp $NETIF $BASEIP)
-
-    cp ${IMAGES}/osv-qemu.img ${IMAGES}/osv-qemu.img.disposible
-
-    ${IMAGES}/osv/root/osv/scripts/imgedit.py setargs \
-	    ${IMAGES}/osv-qemu.img.disposible \
-	    "--rootfs=zfs /redis-server redis.conf"
+    CONTAINER=osv-redis-ramusage
+    docker pull hlefeuvre/osv
+    docker run --privileged --name=$CONTAINER -dt hlefeuvre/osv
+    docker exec $CONTAINER ./scripts/build -j4 fs=ramfs image=redis-memonly
 
     {
+      sleep 4
+      docker container stop $CONTAINER
+      docker rm -f $CONTAINER
       sleep 3
-      ip=`cat $(pwd)/dnsmasq.log | grep "dnsmasq-dhcp: DHCPACK(${NETIF})" | \
-		tail -n 1 | awk  '{print $3}'`
-
-      # benchmark
-      ping -q -c 1 $ip
-      up=$?
-
-      delete_bridge $NETIF
-      kill_dhcp $dnsmasq_pid
-      rm ${IMAGES}/osv-qemu.img.disposible
-      kill_qemu
-
-      exit $up
     } &
 
-    checker_id=$!
+    (docker exec $CONTAINER ./scripts/run.py -m $1) > .out 2> /dev/null
 
-    taskset -c ${CPU1} qemu-guest \
-	    -q ${IMAGES}/osv-qemu.img.disposible \
-	    -m ${1} -p ${CPU2} \
-	    -b ${NETIF} -x
+    wait
 
-    child_pid=$!
-
-    wait $checker_id
+    grep -q "ready to accept connections" .out
     up=$?
-
-    # stop server
-    kill -9 $child_pid
+    rm .out
 
     exit $up
 }
 
 function nginx_osv_with_mem {
-    IMAGES=$(pwd)/images/nginx
-    NETIF=tux0
-    create_bridge $NETIF $BASEIP
-    dnsmasq_pid=$(run_dhcp $NETIF $BASEIP)
-
-    cp ${IMAGES}/osv-qemu.img ${IMAGES}/osv-qemu.img.disposible
-
-    ${IMAGES}/osv/root/osv/scripts/imgedit.py setargs \
-	    ${IMAGES}/osv-qemu.img.disposible \
-	    "--rootfs=ramfs /nginx.so -c /nginx/conf/nginx.conf"
+    CONTAINER=osv-nginx-ramusage
+    docker pull hlefeuvre/osv
+    docker run --privileged --name=$CONTAINER -dt hlefeuvre/osv
+    docker exec $CONTAINER sed -i "s/#error_log stderr debug;/error_log stderr debug;/g" \
+	    apps/nginx/patches/nginx.conf
+    docker exec $CONTAINER ./scripts/build -j4 fs=ramfs image=nginx
 
     {
+      sleep 4
+      docker container stop $CONTAINER
+      docker rm -f $CONTAINER
       sleep 3
-      ip=`cat $(pwd)/dnsmasq.log | grep "dnsmasq-dhcp: DHCPACK(${NETIF})" | \
-		tail -n 1 | awk  '{print $3}'`
-
-      # benchmark
-      ping -q -c 1 $ip
-      up=$?
-
-      delete_bridge $NETIF
-      kill_dhcp $dnsmasq_pid
-      rm ${IMAGES}/osv-qemu.img.disposible
-      kill_qemu
-
-      exit $up
     } &
 
-    checker_id=$!
+    (docker exec $CONTAINER ./scripts/run.py -m $1) > .out 2> /dev/null
 
-    taskset -c ${CPU1} qemu-guest \
-	    -q ${IMAGES}/osv-qemu.img.disposible \
-	    -m $1 -p ${CPU2} \
-	    -b ${NETIF} -x
+    wait
 
-    child_pid=$!
-
-    wait $checker_id
+    grep -q "start worker processes" .out
     up=$?
-
-    # stop server
-    kill -9 $child_pid
+    rm .out
 
     exit $up
 }
