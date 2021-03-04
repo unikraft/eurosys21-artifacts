a4d16ad143f59dd2e3782f47f2dbdc69a3ee72b3
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-03-05 00:40:50 +0100

    tab_01: Separate FS creation from benchmark compiling
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

--- experiments/tab_01_bincompat-syscalls/build-benchmark.sh
+++ experiments/tab_01_bincompat-syscalls/build-benchmark.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 SRC="${1:-.}/src"
-TOOLS="$( readlink -f "${1:-.}/../../tools" )"
 set -e
 set -x
 
 cd "$SRC"
 make clean syscallbench
-
-# Create intrd for linux containing the benchmark
-# Copy the benchmark as init process
-cp -f "syscallbench" "linux-initrd/init"
-"$TOOLS/mkinitrd" "syscallbench.initrd.gz" "linux-initrd/"
 exit $?
