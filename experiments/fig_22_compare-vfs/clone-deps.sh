3c1fbc6035476a5e7800900427d7dee4d74e6d3b
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-03-04 23:17:21 +0100

    fig_22: clone-deps.sh
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

---
+++ experiments/fig_22_compare-vfs/clone-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CLONED="${1:-.}/cloned"
+
+if [ -e "$CLONED" -a ! -d "$CLONED" ]; then
+	echo "Target '$CLONED' does exist but is not a directory"
+	exit 1
+fi
+
+set -e
+
+[ ! -d "$CLONED" ] && mkdir -pv "$CLONED"
+cd "$CLONED"
+
+# clone repos if they do not exist yet
+[ ! -e "unikraft" ] && git clone --branch skuenzer/eurosys21 'https://github.com/skuenzer/unikraft.git'
+[ ! -e "shfs" ]     && git clone --branch skuenzer/eurosys21 'https://github.com/skuenzer/lib-shfs.git' 'shfs'
+[ ! -e "tlsf" ]     && git clone --branch staging 'https://github.com/unikraft/lib-tlsf.git' 'tlsf'
+exit 0
