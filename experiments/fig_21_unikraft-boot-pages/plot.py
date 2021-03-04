7e5c704da4c75b0210de2c7e92a627c9f1f59ffc
Alexander Jung <a.jung@lancs.ac.uk>
2021-03-04 22:51:50 +0100

    fig_21: Parameterize plot
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- experiments/fig_21_unikraft-boot-pages/plot.py
+++ experiments/fig_21_unikraft-boot-pages/plot.py
@@ -7,6 +7,7 @@
 import os
 import csv
 import sys
+import fire
 import numpy as np
 from time import gmtime
 from time import strftime
@@ -18,140 +19,143 @@ from os import listdir, makedirs
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-WORKDIR = os.getcwd()
-RESULTSDIR = WORKDIR + '/results/'
-RESULTEXT = '.csv'
-GROUP_BAR_WIDTH = .4
-DEFAULT = '_'
-THROUGHPUT = 'throughput'
-MEAN_KEY = 'mean'
-MEDIAN_KEY = 'median'
-AMAX_KEY = 'amax'
-AMIN_KEY = 'amin'
+def plot(data=None, output=None):
+  WORKDIR = os.getcwd()
+  RESULTSDIR = data
+  RESULTEXT = '.csv'
+  GROUP_BAR_WIDTH = .4
+  DEFAULT = '_'
+  THROUGHPUT = 'throughput'
+  MEAN_KEY = 'mean'
+  MEDIAN_KEY = 'median'
+  AMAX_KEY = 'amax'
+  AMIN_KEY = 'amin'
 
-files = []
-labels = []
-apps = []
-stats = {}
-throughput_max = 0 # maximum observed throughput
-total_apps = 0
-bar_color = '#5697C4'
+  files = []
+  labels = []
+  apps = []
+  stats = {}
+  throughput_max = 0 # maximum observed throughput
+  total_apps = 0
+  bar_color = '#5697C4'
 
-for f in os.listdir(RESULTSDIR):
-  if f.endswith(RESULTEXT):
-    alloc = f.replace(RESULTEXT,'')
-    
-    if alloc not in stats:
-      stats[alloc] = {}
-    
-    with open(os.path.join(RESULTSDIR, f), 'r') as csvfile:
-      csvdata = csv.reader(csvfile, delimiter="\t")
+  for f in os.listdir(RESULTSDIR):
+    if f.endswith(RESULTEXT):
+      alloc = f.replace(RESULTEXT,'')
       
-      next(csvdata) # skip header
-
-      operations = {}
-
-      for row in csvdata:
-        if row[0] not in operations:
-          operations[int(row[0])] = []
+      if alloc not in stats:
+        stats[alloc] = {}
+      
+      with open(os.path.join(RESULTSDIR, f), 'r') as csvfile:
+        csvdata = csv.reader(csvfile, delimiter="\t")
         
-        operations[int(row[0])].append(float(row[1]))
-      
-      for operation in operations:
-        all_ops = np.array(operations[int(operation)])
-        operations[int(operation)] = {
-          MEAN_KEY: int(round(np.average(all_ops))),
-          MEDIAN_KEY: int(round(np.median(all_ops))),
-          AMAX_KEY: int(round(np.amax(all_ops))),
-          AMIN_KEY: int(round(np.amin(all_ops)))
-        }
+        next(csvdata) # skip header
 
-        if int(round((np.amax(all_ops)))) > throughput_max:
-          throughput_max = int(round((np.amax(all_ops))))
+        operations = {}
 
-      stats[alloc] = operations
+        for row in csvdata:
+          if row[0] not in operations:
+            operations[int(row[0])] = []
+          
+          operations[int(row[0])].append(float(row[1]))
+        
+        for operation in operations:
+          all_ops = np.array(operations[int(operation)])
+          operations[int(operation)] = {
+            MEAN_KEY: int(round(np.average(all_ops))),
+            MEDIAN_KEY: int(round(np.median(all_ops))),
+            AMAX_KEY: int(round(np.amax(all_ops))),
+            AMIN_KEY: int(round(np.amin(all_ops)))
+          }
 
-# General style
-common_style(plt)
+          if int(round((np.amax(all_ops)))) > throughput_max:
+            throughput_max = int(round((np.amax(all_ops))))
 
-throughput_max += 100 # margin above biggest bar
+        stats[alloc] = operations
 
-# Setup matplotlib axis
-fig = plt.figure(figsize=(8, 5))
-renderer = fig.canvas.get_renderer()
+  # General style
+  common_style(plt)
 
-# image size axis
-ax1 = fig.add_subplot(1,1,1)
-ax1.set_ylabel("Boot Time in Microseconds")
-ax1.grid(which='major', axis='y', linestyle=':', alpha=0.5, zorder=0)
-ax1_yticks = np.arange(0, throughput_max, step=50)
-ax1.set_yticks(ax1_yticks, minor=False)
-ax1.set_yticklabels(ax1_yticks)
-ax1.set_ylim(0, throughput_max)
+  throughput_max += 100 # margin above biggest bar
 
-# Plot coordinates
-scale = 1. / len(stats.keys())
-xlabels = []
+  # Setup matplotlib axis
+  fig = plt.figure(figsize=(8, 5))
+  renderer = fig.canvas.get_renderer()
 
-# Adjust margining
-# fig.subplots_adjust(bottom=.15) #, top=1)
+  # image size axis
+  ax1 = fig.add_subplot(1,1,1)
+  ax1.set_ylabel("Boot Time in Microseconds")
+  ax1.grid(which='major', axis='y', linestyle=':', alpha=0.5, zorder=0)
+  ax1_yticks = np.arange(0, throughput_max, step=50)
+  ax1.set_yticks(ax1_yticks, minor=False)
+  ax1.set_yticklabels(ax1_yticks)
+  ax1.set_ylim(0, throughput_max)
 
-i = 0
-line_offset = 0
-for method in ['static', 'dynamic']:
-  operations = stats[method]
+  # Plot coordinates
+  scale = 1. / len(stats.keys())
+  xlabels = []
 
-  # Plot each operation
-  for operation in sorted(operations):
+  # Adjust margining
+  # fig.subplots_adjust(bottom=.15) #, top=1)
 
-    label = '%s %s' % (method, sizeof_fmt(operation * KBYTES * KBYTES))
+  i = 0
+  line_offset = 0
+  for method in ['static', 'dynamic']:
+    operations = stats[method]
 
-    xlabels.append(label)
+    # Plot each operation
+    for operation in sorted(operations):
 
-    print(label, operations[operation][MEAN_KEY])
+      label = '%s %s' % (method, sizeof_fmt(operation * KBYTES * KBYTES))
 
-    # Plot each application
-    bar = ax1.bar([i + 1], operations[operation][MEAN_KEY],
-      label=label,
-      align='center',
-      zorder=4,
-      yerr=operations[operation][AMAX_KEY]-operations[operation][AMIN_KEY],
-      error_kw=dict(lw=2, capsize=10, capthick=1),
-      width=GROUP_BAR_WIDTH,
-      color=bar_color,
-      linewidth=.5
-    )
-  
-    ax1.text(i + 1, operations[operation][AMAX_KEY] + 10, operations[operation][MEAN_KEY],
-      ha='center',
-      va='bottom',
-      zorder=6,
-      fontsize=LARGE_SIZE,
-      linespacing=0,
-      bbox=dict(pad=-.6, facecolor='white', linewidth=0),
-      rotation='vertical'
-    )
+      xlabels.append(label)
 
-    i += 1
+      print(label, operations[operation][MEAN_KEY])
 
-# set up x-axis labels
-xticks = range(1, len(xlabels) + 1)
-ax1.set_xticks(xticks)
-ax1.set_xticklabels(xlabels, fontsize=LARGE_SIZE, rotation=40, ha='right', rotation_mode='anchor')
-# ax1.set_xticklabels(xlabels, fontsize=LARGE_SIZE, fontweight='bold')
-ax1.set_xlim(.5, len(xlabels) + .5)
-ax1.yaxis.grid(True, zorder=0, linestyle=':')
-ax1.tick_params(axis='both', which='both', length=0)
+      # Plot each application
+      bar = ax1.bar([i + 1], operations[operation][MEAN_KEY],
+        label=label,
+        align='center',
+        zorder=4,
+        yerr=operations[operation][AMAX_KEY]-operations[operation][AMIN_KEY],
+        error_kw=dict(lw=2, capsize=10, capthick=1),
+        width=GROUP_BAR_WIDTH,
+        color=bar_color,
+        linewidth=.5
+      )
+    
+      ax1.text(i + 1, operations[operation][AMAX_KEY] + 10, operations[operation][MEAN_KEY],
+        ha='center',
+        va='bottom',
+        zorder=6,
+        fontsize=LARGE_SIZE,
+        linespacing=0,
+        bbox=dict(pad=-.6, facecolor='white', linewidth=0),
+        rotation='vertical'
+      )
 
-# Create a unique legend
-# handles, labels = plt.gca().get_legend_handles_labels()
-# by_label = dict(zip(labels, handles))
-# leg = plt.legend(by_label.values(), by_label.keys(), loc='upper left', ncol=4)
-# leg.get_frame().set_linewidth(0.0)
+      i += 1
 
-plt.setp(ax1.lines, linewidth=.5)
+  # set up x-axis labels
+  xticks = range(1, len(xlabels) + 1)
+  ax1.set_xticks(xticks)
+  ax1.set_xticklabels(xlabels, fontsize=LARGE_SIZE, rotation=40, ha='right', rotation_mode='anchor')
+  # ax1.set_xticklabels(xlabels, fontsize=LARGE_SIZE, fontweight='bold')
+  ax1.set_xlim(.5, len(xlabels) + .5)
+  ax1.yaxis.grid(True, zorder=0, linestyle=':')
+  ax1.tick_params(axis='both', which='both', length=0)
 
-# Save to file
-fig.tight_layout()
-makedirs("../plots", exist_ok=True)
-fig.savefig("../plots/unikraft_boottime_pagesize.pdf") #, bbox_extra_artists=(ax1,), bbox_inches='tight')
+  # Create a unique legend
+  # handles, labels = plt.gca().get_legend_handles_labels()
+  # by_label = dict(zip(labels, handles))
+  # leg = plt.legend(by_label.values(), by_label.keys(), loc='upper left', ncol=4)
+  # leg.get_frame().set_linewidth(0.0)
+
+  plt.setp(ax1.lines, linewidth=.5)
+
+  # Save to file
+  fig.tight_layout()
+  fig.savefig(output) #, bbox_extra_artists=(ax1,), bbox_inches='tight')
+
+if __name__ == '__main__':
+  fire.Fire(plot)
