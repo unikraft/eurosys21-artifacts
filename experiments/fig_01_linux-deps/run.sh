a34b7fcf67655035de64f6d29ccf1c612fd12bca
Alexander Jung <a.jung@lancs.ac.uk>
2021-03-05 23:48:53 +0100

    fig_01: Move clone to prepare step
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- experiments/fig_01_linux-deps/run.sh
+++ experiments/fig_01_linux-deps/run.sh
@@ -2,12 +2,6 @@
 mkdir tmp
 export TMPDIR=$PWD/tmp
 
-git clone https://github.com/torvalds/linux
-
-cd linux && git checkout c85fb28b6f999db9928b841f63f1beeb3074eeca
-
-cd ..
-
 ./create_cscope_db.sh
 
 ./component_symbols.sh
