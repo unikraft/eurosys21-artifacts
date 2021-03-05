aefbd3a791726c6879cb8ef3d2505eca8f69a76c
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-03-05 03:16:10 +0100

    fig_22: Clean
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

---
+++ experiments/fig_22_compare-vfs/clean.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+WORKDIR="${1:-.}"
+SRC="${1:-.}/src"
+
+##
+## benchmark program
+##
+rm -rvf "${SRC}/build"
+rm -rvf "${SRC}/.config"
+rm -vf  "${SRC}/*.o"
+rm -vf  "${SRC}/fsbench"
+
+##
+## intrds and disk images
+##
+rm -vf "${SRC}/uk-initrd/"*
+rm -vf "${SRC}/linux-initrd/"*
+rm -vf "${SRC}/fsbench.shfs"
+rm -vf "${SRC}/fsbench.cpio"
+rm -vf "${SRC}/fsbench.initrd.gz"
+
+##
+## directories
+##
+rm -rvf "${WORKDIR}/cloned"
+rm -rvf "${WORKDIR}/eval"
+rm -rvf "${WORKDIR}/results"
+
+exit $?
