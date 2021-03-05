f448cddc3eae0e5f52e9a3feb9484c33398cc11a
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2021-03-05 22:38:02 +0100

    Get dpdk build remote

--- experiments/tab_04_kvs_compare/clean.sh
+++ experiments/tab_04_kvs_compare/clean.sh
@@ -1,3 +1,4 @@
 sudo python3 aux/dpdk/usertools/dpdk-devbind.py -u 0000:01:00.0
 sudo python3 aux/dpdk/usertools/dpdk-devbind.py -b ixgbe 0000:01:00.0 0000:01:00.1
+rm -rf aux/lib_src
 cd server/unikraft_raw/uk_test_suite/ && make properclean
--- experiments/tab_04_kvs_compare/init.sh
+++ experiments/tab_04_kvs_compare/init.sh
@@ -1,1 +1,5 @@
 # get dpdk
+cd aux
+wget http://vmx.cs.pub.ro/~vmchecker/lib_src.zip
+unzip lib_src
+rm lib_src.zip
