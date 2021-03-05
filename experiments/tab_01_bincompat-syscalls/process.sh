b633aac875a97081435709d8f27c69bc7cf8b732
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-03-05 02:25:12 +0100

    tab_01: Parse and render table
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

---
+++ experiments/tab_01_bincompat-syscalls/process.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PARSED="${1:-.}/eval/parsed"
+RESULTS="${1:-.}/results"
+PROCESS_DS="${1:-.}/do_process_dataset.py"
+
+process()
+{
+	echo "$( basename ${1} )"
+	"${PROCESS_DS}" "${1}"
+	echo ""
+}
+
+mkdir -pv "${RESULTS}"
+process "${PARSED}/linux"		>  "$RESULTS/table.txt"
+process "${PARSED}/linux-nomitig"	>> "$RESULTS/table.txt"
+process "${PARSED}/unikraft"		>> "$RESULTS/table.txt"
