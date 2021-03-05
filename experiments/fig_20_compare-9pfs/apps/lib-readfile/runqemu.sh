a01e3cef12d8270d325eafc9079e48739edc37b0
Cyril Soldani <cyril.soldani@uliege.be>
2021-03-05 13:58:37 +0100

    Avoid sudo in fig. 20
    Signed-off-by: Cyril Soldani <cyril.soldani@uliege.be>

--- experiments/fig_20_compare-9pfs/apps/lib-readfile/runqemu.sh
+++ experiments/fig_20_compare-9pfs/apps/lib-readfile/runqemu.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-sudo sh -c \
+
+if [ $(id -u) -eq 0 ]; then
+    SUDO=
+else
+    SUDO=sudo
+fi
+
+$SUDO sh -c \
     "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 taskset -c 3 qemu-system-x86_64 -cpu host -enable-kvm -nographic -vga none -device isa-debug-exit -fsdev local,id=myid,path=test,security_model=none -device virtio-9p-pci,fsdev=myid,mount_tag=test,disable-modern=on,disable-legacy=off -kernel ./build/lib-helloworld_kvm-x86_64 -append "$1 $2"
 #qemu-system-x86_64 -cpu host -enable-kvm -nographic -vga none -device isa-debug-exit -fsdev local,id=myid,path=test,security_model=none -netdev tap,id=guest0,vhost=on,script=no -device virtio-9p-pci,fsdev=myid,mount_tag=test,disable-modern=on,disable-legacy=off -kernel ./build/lib-helloworld_kvm-x86_64 -append "$1 $2"
