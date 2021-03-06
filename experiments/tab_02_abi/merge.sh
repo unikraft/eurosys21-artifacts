9351048629e870382166bd07c73b4201f1644783
Gaulthier Gain <gaulthier.gain@uliege.be>
2021-03-06 22:09:14 +0100

    tab 02: Fix results
    Signed-off-by: Gaulthier Gain <gaulthier.gain@uliege.be>

--- experiments/tab_02_abi/merge.sh
+++ experiments/tab_02_abi/merge.sh
@@ -14,7 +14,7 @@ else
 fi
 
 CURRENT_FOLDER=$PWD
-DATA_FOLDER="data"
+RESULT_FOLDER="results"
 
 FILE1="$PWD/abi/apps_loc/cloc.csv"
 FILE2="$PWD/abi/apps_musl_compat/musl-compat.csv"
@@ -22,24 +22,24 @@ FILE3="$PWD/abi/apps_newlib_compat/newlib-compat.csv"
 FILE4="$PWD/abi/apps_musl_std/musl-std.csv"
 FILE5="$PWD/abi/apps_newlib_std/newlib-std.csv"
 
-mkdir -p "$DATA_FOLDER"
+mkdir -p "$RESULT_FOLDER"
 
 if [ -f "$FILE1" ]; then
-    cp "$FILE1" "$CURRENT_FOLDER/$DATA_FOLDER"
+    cp "$FILE1" "$CURRENT_FOLDER/$RESULT_FOLDER"
 else
     echo "$FILE1 does not exist. Perform the abi experiment first"
     exit 1
 fi
 
 if [ -f "$FILE2" ]; then
-    cp "$FILE2" "$CURRENT_FOLDER/$DATA_FOLDER"
+    cp "$FILE2" "$CURRENT_FOLDER/$RESULT_FOLDER"
 else
     echo "$FILE1 does not exist. Perform the abi experiment first"
     exit 1
 fi
 
 if [ -f "$FILE3" ]; then
-    cp "$FILE3" "$CURRENT_FOLDER/$DATA_FOLDER"
+    cp "$FILE3" "$CURRENT_FOLDER/$RESULT_FOLDER"
 else
     echo "$FILE3 does not exist. Perform the abi experiment first"
     exit 1
@@ -47,7 +47,7 @@ fi
 
 
 if [ -f "$FILE4" ]; then
-    cp "$FILE4" "$CURRENT_FOLDER/$DATA_FOLDER"
+    cp "$FILE4" "$CURRENT_FOLDER/$RESULT_FOLDER"
 else
     echo "$FILE4 does not exist. Perform the abi experiment first"
     exit 1
@@ -55,7 +55,7 @@ fi
 
 
 if [ -f "$FILE5" ]; then
-    cp "$FILE5" "$CURRENT_FOLDER/$DATA_FOLDER"
+    cp "$FILE5" "$CURRENT_FOLDER/$RESULT_FOLDER"
 else
     echo "$FILE5 does not exist. Perform the abi experiment first"
     exit 1
@@ -66,6 +66,6 @@ python3 merge_csv.py
 echo "Copy results and move to base folder"
 base_folder=$(cat .init_folder.txt)
 cp $PWD/merged.csv $base_folder
-cp -r $DATA_FOLDER $base_folder
+cp -r $RESULT_FOLDER $base_folder
 
-echo "Raw results are available in the data folder and CSV merged table is written in  merged.csv (current folder)"
+echo "Raw results are available in the results folder and CSV merged table is written in merged.csv"
