5872ad665cbdd2db2c4b1a51a592ec6b1cb537e5
Alexander Jung <a.jung@lancs.ac.uk>
2021-03-06 02:10:31 +0100

    README: Update runtime
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- experiments/fig_02_unikraft-nginx-deps/run.sh
+++ experiments/fig_02_unikraft-nginx-deps/run.sh
@@ -1,17 +1,7 @@
+#!/bin/bash
 # Modify tmpdir, on my end it would fail if I was using tmpfs
 mkdir tmp
 export TMPDIR=$PWD/tmp
-
-git clone https://github.com/unikraft/unikraft
-git clone https://github.com/unikraft/lib-lwip
-
-mv lib-lwip unikraft/lib/
-
-
-cd unikraft && git checkout 775dc0123e8523008463fe6b28d45b87a836ae89
-
-cd ..
-mv unikraft/lib/nolibc unikraft/lib/lib-newlib
 
 cscope -b -R
 
