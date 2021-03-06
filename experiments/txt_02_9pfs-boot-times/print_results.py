92740b0877ad88289decd290921c6afca9830780
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2021-03-06 10:38:18 +0100

    Quick fix for 9pfs boot times

--- experiments/txt_02_9pfs-boot-times/print_results.py
+++
@@ -1,1 +0,0 @@
-# takes the averege from each file in results and computes the difference
--- experiments/txt_02_9pfs-boot-times/run_xen.sh
+++ experiments/txt_02_9pfs-boot-times/run_xen.sh
@@ -4,7 +4,7 @@ make
 
 echo "" > xen_9p.results
 echo "With 9pfs (ns)"
-for i in {1..5}
+for i in {1..10}
 do
 sudo xl create -c xl_9p.conf | grep "some_long_computation" | cut -d ' ' -f  4 | tee >> xen_9p.results
 sleep 15
@@ -12,7 +12,7 @@ done
 
 echo "" > xen.results
 echo "Without 9pfs (ns)"
-for i in {1..5}
+for i in {1..10}
 do
 sudo xl create -c xl.conf | grep "some_long_computation" | cut -d ' ' -f  4 | tee >> xen.results
 sleep 15
