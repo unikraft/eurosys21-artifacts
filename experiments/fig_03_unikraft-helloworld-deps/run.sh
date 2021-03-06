a1769df479834fe215135a7f71e8f6017d2a9f80
Alexander Jung <a.jung@lancs.ac.uk>
2021-03-06 02:25:30 +0100

    fig_03: Move clone step to prepare
    Signed-off-by: Alexander Jung <a.jung@lancs.ac.uk>

--- experiments/fig_03_unikraft-helloworld-deps/run.sh
+++ experiments/fig_03_unikraft-helloworld-deps/run.sh
@@ -1,15 +1,7 @@
 # Modify tmpdir, on my end it would fail if I was using tmpfs
 mkdir tmp
 export TMPDIR=$PWD/tmp
-git clone https://github.com/unikraft/unikraft
-
-cd unikraft && git checkout 775dc0123e8523008463fe6b28d45b87a836ae89
-
-cd ..
-
 
 cscope -b -R
 
 ./component_symbols.sh
-
-python3 plot.py
