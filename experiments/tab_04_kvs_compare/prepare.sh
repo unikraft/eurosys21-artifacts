15355c0a5082e248f3c2165aff0291f4e2697ccb
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2021-03-06 18:38:48 +0100

    experiment kvs compare: Multiple fixes

--- experiments/tab_04_kvs_compare/prepare.sh
+++ experiments/tab_04_kvs_compare/prepare.sh
@@ -3,3 +3,12 @@ cd aux
 wget http://vmx.cs.pub.ro/~vmchecker/lib_src.zip
 unzip lib_src
 rm lib_src.zip
+
+wget https://fast.dpdk.org/rel/dpdk-19.11.6.tar.xz
+tar xJf dpdk-19.11.6.tar.xz
+rm dpdk-19.11.6.tar.xz
+mv dpdk-stable-19.11.6/ dpdk2
+cd dpdk2
+meson build
+cd build
+ninja
