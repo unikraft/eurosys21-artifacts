6050cf6d0dcabd1c0975e3d4945dca49dd40475d
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2021-03-07 00:12:11 +0100

    Add more dpdk fixes

--- experiments/tab_04_kvs_compare/server/linux_single/run.sh
+++ experiments/tab_04_kvs_compare/server/linux_single/run.sh
@@ -1,4 +1,5 @@
 gcc server.c -o server
-ip a a 172.17.0.114 dev  enp2s0
-ip a l set enp2s0 up
+sudo arp -s 172.17.0.4 a0:36:9f:52:29:D0
+sudo ip a a 172.17.0.114 dev enp1s0f1
+sudo ip l set enp1s0f1 up
 ./server
